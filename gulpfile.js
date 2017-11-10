71e319048b1694ca7fd74824bbf406184d4e98b1
vinicius nunes <viniiciuspereiira@hotmail.com>
2017-11-10 20:52:16 -0200

    Aplicação rodando no dist

--- gulpfile.js
+++ gulpfile.js
@@ -12,40 +12,46 @@ var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync');
 var minifyCss = require('gulp-minify-css');
+var reload = browserSync.reload;
 
 /* gulp.task('gulp', function(){
   gulp.src('/dev')
   .pipe(gulp().on('error', gulp.logError))
   .pipe(gulp.dest('dist'));
-});
-
- */
+}); */
 
  var paths = {
   sass: ['./dev/sass/*.scss'],
-  script: ['./dev/js/script.js'],
+  script: ['./dev/js/*.js'],
   htmls: ['./dev/**/*.html'],
  };
 
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['server', 'watch', 'html-compile']);
 
 /* Inicia o servidor */
-gulp.task('browser-sync', function(){
+gulp.task('server', function(){
   browserSync.init({
     server: {
-      baseDir: 'dev'
+      baseDir: 'dist'
     }
   });
 });
 
-/* compilando sass para css e salvando em nova pasta */
+/* compilando sass para css e salvando no dist */
 gulp.task('sass-compile', function() {
   return gulp.src('./dev/sass/style.scss')
     .pipe(sass()).on('error', sass.logError)
-    .pipe(gulp.dest('./dev/css'));
+    .pipe(gulp.dest('./dist/css'));
+});
+
+/* passando o html para o dist */
+gulp.task('html-compile', function(){
+  gulp.src('./dev/*.html')
+  .pipe(gulp.dest('./dist'));
 });
 
 /* Aguarda por alterações nos arquivos */
 gulp.task('watch', function(){
-  gulp.watch(paths.sass, ['sass-compile']);  
+  gulp.watch(paths.sass, ['sass-compile']).on('change', reload);
+  gulp.watch(paths.htmls, ['html-compile']).on('change', reload);
 });
